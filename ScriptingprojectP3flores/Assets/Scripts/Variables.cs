eee76e470d4cc3a526bcb73cf63aaa5051518240
Jorge Flores <720624@student.dusd.net>
2023-11-02 10:59:11 -0700

    Made so many scripts
    I have made scenes and scripts to different folders

---
+++ ScriptingprojectP3flores/Assets/Scripts/Variables.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    int myInt = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInt = MultpilyByTwo(myInt);
+        Debug.Log (myInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    int MultpilyByTwo    (int num)
+       
+    {
+        int result;
+        result = num * 2;
+        return result;
+    }
+}
