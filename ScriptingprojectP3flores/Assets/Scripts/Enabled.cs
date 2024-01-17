0f7711bfce349ad334919e08d4500ce52631a083
Jorge Flores <720624@student.dusd.net>
2024-01-17 11:41:43 -0800

    I made a new script into my game
    new script called enabling and disabling

---
+++ ScriptingprojectP3flores/Assets/Scripts/Enabled.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enabled : MonoBehaviour
+{
+    private Light myLight;
+
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            myLight.enabled = !myLight.enabled;
+        } 
+    }
+}
