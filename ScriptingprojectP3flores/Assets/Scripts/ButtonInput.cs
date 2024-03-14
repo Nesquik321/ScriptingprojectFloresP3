c0fdeae371084fe366c3398e043a6cb008a89959
Jorge Flores <720624@student.dusd.net>
2024-03-14 10:56:41 -0700

    I added so many scripts to this project
    New scripts are from GetComponent

---
+++ ScriptingprojectP3flores/Assets/Scripts/ButtonInput.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInput : MonoBehaviour
+{
+    
+    public Text boolDisplay1;
+    public Text boolDisplay2;
+    public Text boolDisplay3;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        bool down = Input.GetButtonDown("Jump");
+        bool held = Input.GetButton("Jump");
+        bool up = Input.GetButtonUp("Jump");
+
+        boolDisplay1.text = "" + down;
+        boolDisplay2.text = "" + held;
+        boolDisplay3.text = "" + held;
+    }
+}
